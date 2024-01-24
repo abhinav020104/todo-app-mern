853abcfcd105df27a975f883ab8b17dbddd67f37
Chinmay Jain <chinmayluhadiya62@gmail.com>
2024-01-24 09:47:56 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
+import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
 import './App.css'
 
 function App() {
 
+  const [todos, setTodos] = useState([]);
+
+  fetch("http://localhost:3000/todos")
+  .then(async function(res){
+    const json = await res.json();
+    setTodos(json.todos);
+  })
+
   return (
     <>
       <CreateTodo></CreateTodo>
-      <Todos></Todos>
+      <Todos todos={[
+        {
+          title: "gym",
+          description: "go to gym",
+          completed: false
+        },
+        {
+          title: "khana",
+          description:"khaalo guys",
+          completed: true
+        }
+      ]}></Todos>
     </>
   )
 }
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,47 @@
-export function Todos(){
-    return
-    <div>
-        <h1>Gym</h1>
-        <h2>go to gym</h2>
-        <button>mark as completed</button>
+import { useState } from "react";
+
+export function CreateTodo(){
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    return <div>  
+        <input style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="title" onChange={
+            function(e){
+                const tvalue = e.target.value;
+                setTitle(tvalue);
+            }
+        }></input><br/>
+        <input style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="description" onChange={
+            function(e){
+                const dvalue = e.target.value;
+                setDescription(dvalue);
+            }
+        }></input>
+        <br />
+        <button style={{
+            padding: 10, 
+            margin: 10}}
+            onClick={()=>{
+                fetch("http://localhost:3000/todo", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        title: title,
+                        description: description
+                    }),
+                    headers:{
+                        "contentType": "application/json"
+                    }
+                })
+                .then(async function(res){
+                    const json = await res.json();
+                    alert("todo added");
+                })
+            }}>Add a TODO</button>
     </div>
 }
